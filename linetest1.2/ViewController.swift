2d477eacf34f50c622817d67f83f52fc1718e654
david crabtree <david@Davids-MacBook-Pro.local>
2017-01-27 17:18:30 -0500

    Initial Commit

---
+++ linetest1.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  linetest1.2
+//
+//  Created by david crabtree on 1/27/17.
+//  Copyright © 2017 david crabtree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
